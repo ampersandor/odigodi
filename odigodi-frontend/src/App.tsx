be917d3bd63351d3c86ded78597388c75513a0ad
DongHunKim <ddong3525@gmail.com>
2023-08-15 15:41:23 +0900

    Add contents to Tips page

--- odigodi-frontend/src/App.tsx
+++ odigodi-frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Contact from "./pages/Contact/Contact";
 import Home from "./pages/Home";
+import Tips from "./pages/Tips/Tips";
 
 
 class App extends Component {
@@ -14,6 +15,7 @@ class App extends Component {
         <Navbar/>
           <Routes>
             <Route path='/' element={<Home/>} />
+            <Route path='/tips' element={<Tips/>} />
             <Route path='/Contact' element={<Contact />} />
           </Routes>
       </>
