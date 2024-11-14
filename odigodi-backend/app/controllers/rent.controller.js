ceb8a9d937d37f0316ead4544e73d5973b98676e
DongHunKim <ddong3525@gmail.com>
2024-11-14 14:28:37 +0900

    filter out the 반전세 & 월세

--- odigodi-backend/app/controllers/rent.controller.js
+++ odigodi-backend/app/controllers/rent.controller.js
@@ -6,8 +6,14 @@ const Op = db.Sequelize.Op;
 exports.findByName = (req, res) => {
   const offinm = req.params.name;
 
-  var condition = offinm ? { offinm: { [Op.iLike]: `%${offinm}%` } } : null;
-  
+  var condition = offinm 
+  ? { 
+      offinm: { [Op.iLike]: `%${offinm}%` },
+      monthlyrent: 0  // 월세가 0인 조건 추가
+    } 
+  : { monthlyrent: 0 };  // 이름 검색이 없을 때도 월세 0인 조건 적용
+
+
   Rent.findAll({ where: condition })
     .then(data => {
       res.send(data);
